1a752749132d8fe3c831bc75e41500f6328fb5b6
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-01-16 23:23:21 +0100

    Finished task one

--- Øvinger/oving_1/src/main.rs
+++ Øvinger/oving_1/src/main.rs
@@ -8,52 +8,51 @@ fn main() {
 
     //If from is greater than to, they swap values
     let from = if to_input < from_input {to_input} else {from_input};
-    let to = if to_input < from_input {from_input} else {to_input};
+    let mut to = if to_input < from_input {from_input} else {to_input};
 
-    //Getting the number of threads from user with a max of 32
-    let number_of_threads:u32 = get_valid_number(from, to);
+    if to % 2 != 0 {to += 1;}
 
-    let index_at_mutex_arc = Arc::new(Mutex::new(from));
-    let list_of_primes_mutex_arc = Arc::new(Mutex::new(Vec::new()));
+    //Number of threads, max should be 2 because my computer only have 2 cores
+    //But max is set to 32 for testing purposes
+    let number_of_threads:u32 = get_valid_number();
+
+    let primes = Arc::new(Mutex::new(Vec::new()));
 
     //Vector of threads
-    let mut vec = Vec::new();
+    let mut threads = Vec::new();
     for i in 0..number_of_threads {
         //Cloning variables because of closure after moving
-        let index_at_mutex_arc_copy = index_at_mutex_arc.clone();
-        let list_of_primes_mutex_arc_copy = list_of_primes_mutex_arc.clone();
-    
-        println!("Making thread: {}", i);
-        let t = thread::spawn(move || {
-            let mut index_at_locked = index_at_mutex_arc_copy.lock().unwrap();
-            println!("Locking at thread: {}", i);
-            while *index_at_locked < to + 1 {
-                // Accessing and locking the data
-                let mut list_of_primes_locked = list_of_primes_mutex_arc_copy.lock().unwrap();
-                if is_prime(*index_at_locked) {list_of_primes_locked.push(*index_at_locked);} 
-                *index_at_locked += 1;
-                // Lock is released at end of scope
+        let primes_copy = primes.clone();
+        let step = number_of_threads;
+        let start = from + i;
+
+        threads.push(thread::spawn(move || {
+            let mut i = start;
+            while i <= to {
+                if is_prime(i) {primes_copy.lock().unwrap().push(i);}
+                i += step;
             }
-            println!("Finished thread: {}\n", i);
-        });
-       vec.push(t);
+            })
+        );
     }
 
     //Joining all the threads
-    for v in vec {
-        v.join();
+    for t in threads {
+        match t.join() {
+            Ok(_) => continue,
+            Err(_) => println!("Thread could not join.."),
+        };
     }
 
     // Cannot access data without calling lock(), even though locking is unnecessary.
-    let list = &mut *list_of_primes_mutex_arc.lock().unwrap();
-    //Do not need to sort the list because the list is already sorted
-    //&list.sort();
+    let list = &mut *primes.lock().unwrap();
+    list.sort();
     //Printing the result list
     for i in list { print!("{}, ", i);}
 }
 
 fn is_prime(n:u32) -> bool {
-    for i in 2..n {
+    for i in 2..n/2 {
         if n % i == 0 {
             return false;
         }
@@ -73,13 +72,17 @@ fn get_number_from_user(message:&str) -> u32 {
     }
 }
 
-fn get_valid_number(from:u32, to:u32 ) -> u32 {
-    let max_number_of_threads:u32 = to - from - 1;
+fn get_valid_number() -> u32{
+    const MAX_NUMBER_OF_THREADS:u32 = 32;
     loop {
-        let number_of_threads:u32 = get_number_from_user("Number of threads");
-        if number_of_threads > max_number_of_threads {
-            continue;
-        }
-        return number_of_threads;
+        println!("Number of threads (Max 32) ");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Could not receive input");
+        match input.trim().parse() {
+            Ok(num) => {
+                if num > MAX_NUMBER_OF_THREADS {continue;}
+                return num },
+            Err(_) => continue,
+        };
     }
 }
