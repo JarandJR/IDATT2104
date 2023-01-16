a4d2816dae4cd41d60aecf58bd191189186777a9
JarandJR <114679050+JarandJR@users.noreply.github.com>
2023-01-16 12:15:19 +0100

    Delete Øvinger/oving_1/target directory

--- Øvinger/oving_1/target/debug/deps/oving_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jaran\Documents\Nettverksprog\IDATT2104\Øvinger\oving_1\target\debug\deps\oving_1.exe: src\main.rs
-
-C:\Users\jaran\Documents\Nettverksprog\IDATT2104\Øvinger\oving_1\target\debug\deps\oving_1.d: src\main.rs
-
-src\main.rs:
--- Øvinger/oving_1/target/debug/oving_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\jaran\Documents\Nettverksprog\IDATT2104\Øvinger\oving_1\target\debug\oving_1.exe: C:\Users\jaran\Documents\Nettverksprog\IDATT2104\Øvinger\oving_1\src\main.rs
