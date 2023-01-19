d0ad2c8583a6f25b339c7600f5e92ab93f79cce8
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-01-19 10:49:58 +0100

    formatted the code a bit

--- Øvinger/oving_1/src/main.rs
+++ Øvinger/oving_1/src/main.rs
@@ -3,18 +3,28 @@ use std::thread;
 
 fn main() {
     println!("Find primes between: ");
-    let from_input:u32 = get_number_from_user("from: ");
-    let to_input:u32 = get_number_from_user("to: ");
+    let from_input: u32 = get_number_from_user("from: ");
+    let to_input: u32 = get_number_from_user("to: ");
 
     //If from is greater than to, they swap values
-    let from = if to_input < from_input {to_input} else {from_input};
-    let mut to = if to_input < from_input {from_input} else {to_input};
+    let from: u32 = if to_input < from_input {
+        to_input
+    } else {
+        from_input
+    };
+    let mut to: u32 = if to_input < from_input {
+        from_input
+    } else {
+        to_input
+    };
 
-    if to % 2 != 0 {to += 1;}
+    if to % 2 != 0 {
+        to += 1;
+    }
 
     //Number of threads, max should be 2 because my computer only have 2 cores
     //But max is set to 32 for testing purposes
-    let number_of_threads:u32 = get_valid_number();
+    let number_of_threads: u32 = get_valid_number();
 
     let primes = Arc::new(Mutex::new(Vec::new()));
 
@@ -29,11 +39,12 @@ fn main() {
         threads.push(thread::spawn(move || {
             let mut i = start;
             while i <= to {
-                if is_prime(i) {primes_copy.lock().unwrap().push(i);}
+                if is_prime(i) {
+                    primes_copy.lock().unwrap().push(i);
+                }
                 i += step;
             }
-            })
-        );
+        }));
     }
 
     //Joining all the threads
@@ -48,11 +59,13 @@ fn main() {
     let list = &mut *primes.lock().unwrap();
     list.sort();
     //Printing the result list
-    for i in list { print!("{}, ", i);}
+    for i in list {
+        print!("{}, ", i);
+    }
 }
 
-fn is_prime(n:u32) -> bool {
-    for i in 2..n/2 {
+fn is_prime(n: u32) -> bool {
+    for i in 2..n / 2 {
         if n % i == 0 {
             return false;
         }
@@ -60,11 +73,13 @@ fn is_prime(n:u32) -> bool {
     true
 }
 
-fn get_number_from_user(message:&str) -> u32 {
+fn get_number_from_user(message: &str) -> u32 {
     loop {
         println!("{}", message);
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).expect("Could not receive input");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Could not receive input");
         match input.trim().parse() {
             Ok(num) => return num,
             Err(_) => continue,
@@ -72,16 +87,21 @@ fn get_number_from_user(message:&str) -> u32 {
     }
 }
 
-fn get_valid_number() -> u32{
-    const MAX_NUMBER_OF_THREADS:u32 = 32;
+fn get_valid_number() -> u32 {
+    const MAX_NUMBER_OF_THREADS: u32 = 32;
     loop {
         println!("Number of threads (Max 32) ");
         let mut input = String::new();
-        std::io::stdin().read_line(&mut input).expect("Could not receive input");
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Could not receive input");
         match input.trim().parse() {
             Ok(num) => {
-                if num > MAX_NUMBER_OF_THREADS {continue;}
-                return num },
+                if num > MAX_NUMBER_OF_THREADS {
+                    continue;
+                }
+                return num;
+            }
             Err(_) => continue,
         };
     }
