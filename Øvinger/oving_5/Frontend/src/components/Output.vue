f36dc7d5e7162a8cab21a13a71d56269bac85cee
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-02-17 03:33:43 +0100

    Cleaned up the project and made a spimple layout for the frontend

---
+++ Øvinger/oving_5/Frontend/src/components/Output.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>Output:</p>
+  <div>
+    <p id="output" >{{result}}</p>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+let result = ref("result...");
+
+function printOutput(event) {
+  result = "ur gat"
+}
+</script>
+
+<style scoped>
+div {
+  height: 20rem;
+  width: 100%;
+  background-color: lightskyblue;
+}
+
+#output {
+  /*Does nothing???*/
+  border-width: 10px;
+  border-color: #2c3e50;
+}
+</style>
