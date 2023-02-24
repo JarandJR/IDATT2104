c964e95009a839aba399e0174b19d6371cc6b1bc
Jarand Romestrand <jarandjr@stud.ntnu.no>
2023-02-24 16:04:13 +0100

    Frontend can now send a string of code to the backend. Backend can run terminal code

--- Øvinger/oving_5/Backend/src/main.rs
+++ Øvinger/oving_5/Backend/src/main.rs
@@ -16,7 +16,18 @@ async fn main() -> std::io::Result<()> {
 
 #[post("/post_code")]
 async fn post_code(request: String) -> Json<String> {
-    Json::from(Json(request))
+    println!("Request: {}", request);
+    let command = format!("echo {}", &request);
+    let command = "ping 8.8.8.8";
+    let output = std::process::Command::new("cmd")
+                .args(["/C", &command])
+                .output()
+                .expect("failed to execute process");
+
+    let response = std::str::from_utf8(&output.stdout).expect("Could not parse");
+    println!("{}", response);
+    
+    Json::from(Json(response.to_string()))
 }
 
 #[get("/get_code")]
